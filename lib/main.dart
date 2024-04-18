4998b92eca5d1689f3d92759ce2e729cb6320f8e
Yonas_Mulugeta <yoniwin.yw@gmail.com>
2024-04-18 08:25:57 +0000

    it startts from home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:ui_13/page/home_page.dart';
 
+import 'page/splash_page.dart';
 import 'page/splash_page.dart';
 
 void main() {
@@ -13,14 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Petree',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Gilroy',
       ),
       home: const SafeArea(
-        child: SplashPage(),
+        child: HomePage(),
       ),
     );
   }
